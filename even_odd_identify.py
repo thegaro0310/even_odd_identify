889f85561eff3930be2c3832eb3fabb172b322d7
thegaro0310 <113992891+thegaro0310@users.noreply.github.com>
2022-09-20 19:36:51 +0700

    Create even_odd_identify.py
    Develop a program to identify 10 random integers

---
+++ even_odd_identify.py
@@ -0,0 +1,39 @@
+import random
+
+# declare for randomlist
+randomlist = []
+# declare for loopstate
+loopstate = []
+
+# function to create the number list
+def even_odd_create(arg1):
+    # create random number
+    randomlist = random.sample(range(0,1000), arg1)
+    # then print it out the screen
+    even_odd_print(randomlist)
+    # then return the value
+    return randomlist
+
+# function to print out the number list
+def even_odd_print(arg2):
+    print ('this is the number list:', arg2)
+
+# main function
+def main():
+    # declare value for step_num
+    step_num = 0
+    # pass the value to even_odd_create function
+    loopstate = even_odd_create(10)
+    # main loop
+    for num in loopstate:
+        # increase step_num by 1
+        step_num += 1
+        # condition for even
+        if (num % 2) == 0: 
+            print ('step number:' , step_num , ':' , num , "is an even number")
+        # condition for odd
+        else:
+            print ('step number:' , step_num , ':' , num , "is an odd number")
+
+# call main function
+main()
